f670cfe4b2376cd173bf9402e3ae5835b7265554
SKTT1Ryze <ccc@neos.moe>
2023-10-11 23:09:54 +0900

    register add_two_numbers

--- runtime/src/registration.rs
+++ runtime/src/registration.rs
@@ -6,5 +6,8 @@ pub fn register_all(handle: ContainerHandle) -> anyhow::Result<()> {
     handle.register_problem(|_| problems::two_sum::TwoSum)?;
     handle.register_solution(|_| solutions::two_sum::TwoSum)?;
 
+    handle.register_problem(|_| problems::add_two_numbers::AddTwoNumbers)?;
+    handle.register_solution(|_| solutions::add_two_numbers::AddTwoNumbers)?;
+
     Ok(())
 }
