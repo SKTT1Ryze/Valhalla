41603f309c0fedcf839daa531eac2bde62c77f2b
SKTT1Ryze <ccc@neos.moe>
2023-10-12 21:39:23 +0900

    register LongestSubstring

--- runtime/src/registration.rs
+++ runtime/src/registration.rs
@@ -9,5 +9,8 @@ pub fn register_all(handle: ContainerHandle) -> anyhow::Result<()> {
     handle.register_problem(|_| problems::add_two_numbers::AddTwoNumbers)?;
     handle.register_solution(|_| solutions::add_two_numbers::AddTwoNumbers)?;
 
+    handle.register_problem(|_| problems::longest_substring::LongestSubstring)?;
+    handle.register_solution(|_| solutions::longest_substring::LongestSubstring)?;
+
     Ok(())
 }
