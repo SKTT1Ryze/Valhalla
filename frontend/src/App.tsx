60fc4a475afe0a1b4fcc3f112c12a051295c9fc5
SKTT1Ryze <ccc@neos.moe>
2023-10-13 17:38:29 +0900

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,26 @@
 import './App.css'
 
+interface Item {
+  id: number;
+  difficulty: string;
+  topic: string;
+  title: string;
+  description: string;
+  labels: string[];
+}
+
+const items: Item[] = [
+  {id: 1, difficulty: "", topic: "", title: "Two Numbers", description: "", labels: []}
+];
+
 function App() {
   return (
-    <div>FrontEnd</div>
+    <>
+    <div>LeetCode Solutions of CCC</div>
+    {
+      items.map(item => <div>{item.title}</div>)
+    }
+    </>
   )
 }
 
