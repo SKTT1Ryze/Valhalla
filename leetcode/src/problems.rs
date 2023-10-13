8007a16421e14eeb88cc576f79818483d518bcbf
SKTT1Ryze <ccc@neos.moe>
2023-10-13 23:08:45 +0900

    update runtime

--- leetcode/src/problems.rs
+++ leetcode/src/problems.rs
@@ -6,13 +6,32 @@ pub mod two_sum;
 pub enum Difficulty {
     Easy,
     Medium,
-    Diffcult,
+    Hard,
+}
+
+impl Into<String> for Difficulty {
+    fn into(self) -> String {
+        match self {
+            Self::Easy => "easy".into(),
+            Self::Medium => "Medium".into(),
+            Self::Hard => "Hard".into()
+        }
+    }
 }
 
 #[allow(dead_code)]
 pub enum Topic {
     Algorithms,
     DataBase,
+}
+
+impl Into<String> for Topic {
+    fn into(self) -> String {
+        match self {
+            Self::DataBase => "DataBase".into(),
+            Self::Algorithms => "Algorithms".into()
+        }
+    }
 }
 
 pub trait Problem: Send + Sync {
