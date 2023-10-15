84645a932e7f9dcc016fbf89c60e0fe7793f222e
SKTT1Ryze <ccc@neos.moe>
2023-10-15 22:10:35 +0900

    add problem longest_palindromic_substring

--- runtime/src/registration.rs
+++ runtime/src/registration.rs
@@ -16,6 +16,9 @@ pub fn register_all(handle: ContainerHandle) -> anyhow::Result<()> {
     handle.register_problem(|_| problems::median_of_two_sorted_arrays::MedianOfTwoSortedArrays)?;
     handle
         .register_solution(|_| solutions::median_of_two_sorted_arrays::MedianOfTwoSortedArrays)?;
+    handle.register_problem(|_| {
+        problems::longest_palindromic_substring::LongestPalindromicSubstring
+    })?;
 
     Ok(())
 }
