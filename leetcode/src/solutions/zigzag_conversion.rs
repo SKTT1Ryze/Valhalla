a41c73db0c244ee7ceeae5bf732d36dfca76dc2c
SKTT1Ryze <ccc@neos.moe>
2023-10-23 22:02:21 +0900

    impl solution zigzag_conversion

--- leetcode/src/solutions/zigzag_conversion.rs
+++ leetcode/src/solutions/zigzag_conversion.rs
@@ -16,6 +16,7 @@ impl Solution for SolutionImpl {
         let testcases = [
             ("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR"),
             ("PAYPALISHIRING", 4, "PINALSIGYAHRPI"),
+            ("ABCD", 2, "ACBD"),
             ("A", 1, "A"),
         ];
 
@@ -38,6 +39,76 @@ impl Solution for SolutionImpl {
 
 impl SolutionImpl {
     pub fn convert(s: String, num_rows: i32) -> String {
-        todo!()
+        if num_rows <= 1 {
+            return s;
+        }
+        if num_rows == 2 {
+            return [
+                s.chars()
+                    .into_iter()
+                    .enumerate()
+                    .filter_map(|(i, ch)| if i % 2 == 0 { Some(ch) } else { None })
+                    .collect::<Vec<_>>(),
+                s.chars()
+                    .into_iter()
+                    .enumerate()
+                    .filter_map(|(i, ch)| if i % 2 != 0 { Some(ch) } else { None })
+                    .collect(),
+            ]
+            .into_iter()
+            .flatten()
+            .collect();
+        }
+        let num_rows = num_rows as usize;
+        let num_cols = Self::cal_column(s.len(), num_rows);
+        let mut matrix: Vec<Vec<char>> = vec![vec!['\n'; num_cols]; num_rows];
+        let mut x = 0;
+        let mut y = 0;
+        let mut down = true;
+        let chars: Vec<_> = s.chars().collect();
+
+        for ch in chars {
+            matrix[y][x] = ch;
+
+            if down {
+                if y == num_rows - 1 {
+                    x += 1;
+                    y -= 1;
+                    down = false;
+                } else {
+                    y += 1;
+                }
+            } else {
+                if y == 0 {
+                    x += 1;
+                    y += 1;
+                    down = true;
+                } else {
+                    y -= 1;
+                }
+            }
+        }
+
+        matrix
+            .into_iter()
+            .flatten()
+            .filter(|&ch| ch != '\n')
+            .collect()
+    }
+
+    fn cal_column(s_len: usize, num_rows: usize) -> usize {
+        if num_rows <= 1 {
+            return s_len;
+        }
+        let base = s_len / 2;
+        let residue = s_len % (num_rows * 2 - 2);
+
+        if residue == 0 {
+            return base;
+        } else if residue <= num_rows {
+            return base + 1;
+        } else {
+            return base + 1 + residue - num_rows;
+        }
     }
 }
