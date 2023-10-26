65f2e0a5234f4949d7eab09ef30258e24cf21b66
SKTT1Ryze <ccc@neos.moe>
2023-10-26 21:11:30 +0900

    impl solution string to integer

--- leetcode/src/solutions/string_to_integer.rs
+++ leetcode/src/solutions/string_to_integer.rs
@@ -13,7 +13,12 @@ impl Solution for SolutionImpl {
         crate::location!();
     }
     fn test(&self) -> anyhow::Result<()> {
-        let testcases = [("42", 42), ("   -42", -42), ("4193 with words", 4193)];
+        let testcases = [
+            ("42", 42),
+            ("   -42", -42),
+            ("4193 with words", 4193),
+            ("    10522545459", 2147483647),
+        ];
 
         for (s, expect) in testcases {
             let output = Self::my_atoi(s.to_string());
@@ -30,8 +35,74 @@ impl Solution for SolutionImpl {
     }
 }
 
+#[derive(PartialEq, PartialOrd)]
+enum State {
+    PositiveUnMatched,
+    PositiveMatched,
+    DigitMatched,
+}
+
 impl SolutionImpl {
     pub fn my_atoi(s: String) -> i32 {
-        todo!()
+        let mut state = State::PositiveUnMatched;
+        let mut positive = true;
+        let mut v = Vec::new();
+
+        for ch in s.chars() {
+            match ch {
+                '-' | '+' => {
+                    if state == State::PositiveUnMatched {
+                        positive = ch == '+';
+                        state = State::PositiveMatched;
+                    } else {
+                        break;
+                    }
+                }
+                '0'..='9' => {
+                    if state != State::DigitMatched {
+                        state = State::DigitMatched;
+                    }
+
+                    if ch == '0' && v.is_empty() {
+                        continue;
+                    }
+
+                    v.push(ch as i32 - 48);
+                }
+                ' ' => {
+                    if state != State::PositiveUnMatched {
+                        break;
+                    }
+                    // or ignore
+                }
+                _ => {
+                    if state == State::DigitMatched {
+                        break;
+                    } else {
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        let mut acc = 0;
+        for (p, e) in v.into_iter().rev().enumerate() {
+            acc = Self::cal_overflow(acc, e, p, positive).unwrap_or(if positive {
+                i32::MAX
+            } else {
+                i32::MIN
+            });
+        }
+
+        acc
+    }
+    fn cal_overflow(acc: i32, e: i32, p: usize, positive: bool) -> Option<i32> {
+        let pow = 10_i32.checked_pow(p as u32)?;
+        let pow = e.checked_mul(pow)?;
+        if positive {
+            acc.checked_add(pow)
+        } else {
+            acc.checked_sub(pow)
+        }
     }
 }
