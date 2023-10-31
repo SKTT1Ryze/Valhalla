02d1c2df5d05e6b295516194511cb257f295970c
SKTT1Ryze <ccc@neos.moe>
2023-10-31 21:46:03 +0900

    impl Roman to Integer

--- leetcode-cc/RomanToInteger.hpp
+++ leetcode-cc/RomanToInteger.hpp
@@ -1,3 +1,6 @@
+#include <numeric>
+#include <unordered_map>
+
 #include "problem.h"
 #include "solution.h"
 
@@ -22,8 +25,8 @@ class SRomanToInt : public ISolution {
     auto testCases = {std::make_pair(3, "III"), std::make_pair(58, "LVIII"),
                       std::make_pair(1994, "MCMXCIV")};
 
-    for (const auto& [expect, num] : testCases) {
-      auto output = this->romanToInt(num);
+    for (const auto& [expect, s] : testCases) {
+      auto output = this->romanToInt(s);
 
       if (output != expect) {
         return 1;
@@ -34,5 +37,22 @@ class SRomanToInt : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  int romanToInt(std::string s) const { return 0; }
+  int romanToInt(std::string s) const {
+    std::vector<int> res;
+    std::unordered_map<char, int> map = {
+        {'I', 1},   {'V', 5},   {'X', 10},   {'L', 50},
+        {'C', 100}, {'D', 500}, {'M', 1000},
+    };
+
+    for (const auto& ch : s) {
+      if ((!res.empty()) && ((res.back() == 1 && (ch == 'V' || ch == 'X')) ||
+                             (res.back() == 10 && (ch == 'L' || ch == 'C')) ||
+                             (res.back() == 100 && (ch == 'D' || ch == 'M')))) {
+        res[res.size() - 1] *= -1;
+      }
+      res.push_back(map[ch]);
+    }
+
+    return std::accumulate(res.begin(), res.end(), 0);
+  }
 };
