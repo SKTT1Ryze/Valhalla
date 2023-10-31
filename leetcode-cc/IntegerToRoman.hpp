0b36785e78e0558e7230001464c8fa65708fe641
SKTT1Ryze <ccc@neos.moe>
2023-10-31 10:59:20 +0900

    add Integer To Roman

---
+++ leetcode-cc/IntegerToRoman.hpp
@@ -0,0 +1,23 @@
+#include "problem.h"
+#include "solution.h"
+
+class PIntToRoman : public IProblem {
+ public:
+  std::size_t id() const override { return 12; }
+  int difficulty() const override { return DIFFI_MEDIUM; }
+  int topic() const override { return TOPIC_ALGORITHMS; }
+  std::string title() const override { return "Integer to Romain"; }
+  std::string description() const override { return ""; }
+  std::vector<std::string> labels() const override {
+    return {"Hash Map", "Math", "String"};
+  }
+};
+
+class SIntToRoman : public ISolution {
+ public:
+  std::size_t problemId() const override { return 12; }
+  std::string name() const override { return "Solution for Integer to Romain"; }
+  std::string location() const override { return __FILE__; }
+  int test() const override { return 0; }
+  int benchmark() const override { return 0; }
+};
--- runtime-cc/src/registration.cc
+++ runtime-cc/src/registration.cc
@@ -1,12 +1,12 @@
 #include "registration.h"
 
-#include "../leetcode-cc/example.h"
+#include "../leetcode-cc/IntegerToRoman.hpp"
 
 const int registerAll(std::shared_ptr<Container> handle) {
   handle->registerProblem(
-      []() -> ArcProblem { return std::make_shared<PExample>(); });
+      []() -> ArcProblem { return std::make_shared<PIntToRoman>(); });
   handle->registerSolution(
-      []() -> ArcSolution { return std::make_shared<SExample>(); });
+      []() -> ArcSolution { return std::make_shared<SIntToRoman>(); });
 
   return 0;
 }
