9ce5eb9fc67e6393273b2efacf2dc57dbcb71a5f
Chunchi Che <ccc@neos.moe>
2023-11-01 23:40:03 +0900

    3sum (#27)
    * add Three Sum
    
    * update
    
    * impl Three Sum

---
+++ leetcode-cc/ThreeSum.hpp
@@ -0,0 +1,90 @@
+#include <algorithm>
+#include <set>
+#include <unordered_set>
+#include <utility>
+
+#include "problem.h"
+#include "solution.h"
+
+using namespace std;
+
+class PThreeSum : public IProblem {
+  size_t id() const override { return 15; }
+  int difficulty() const override { return DIFFI_MEDIUM; }
+  int topic() const override { return TOPIC_ALGORITHMS; }
+  string title() const override { return "3Sum"; }
+  string description() const override { return ""; }
+  vector<string> labels() const override { return {"Array", "DP"}; }
+};
+
+class SThreeSum : public ISolution {
+ public:
+  size_t problemId() const override { return 15; }
+  string name() const override { return "Solution for 3Sum"; }
+  string location() const override { return __FILE_NAME__; }
+  int test() const override {
+    auto testCases = {
+        make_pair<vector<int>, vector<vector<int>>>({-1, 0, 1, 2, -1, -4},
+                                                    {{-1, -1, 2}, {-1, 0, 1}}),
+        make_pair<vector<int>, vector<vector<int>>>({0, 1, 1}, {}),
+        make_pair<vector<int>, vector<vector<int>>>({0, 0, 0}, {{0, 0, 0}})};
+
+    for (const auto& [nums, expect] : testCases) {
+      auto output = this->threeSum(nums);
+
+      if (expect.size() != output.size() ||
+          !is_permutation(expect.begin(), expect.end(), output.begin(),
+                          this->compareVectors)) {
+        return 1;
+      }
+    }
+
+    return 0;
+  }
+  int benchmark() const override { return 0; }
+
+ private:
+  vector<vector<int>> threeSum(const vector<int>& nums) const {
+    // TODO: use the two pointer
+    auto len = nums.size();
+    if (len < 3) {
+      return {};
+    }
+    unordered_set<int> map = {};
+    set<vector<int>> res = {};
+    vector<vector<int>> output = {};
+
+    auto sortedNums = nums;
+    sort(sortedNums.begin(), sortedNums.end());
+    vector<int> prev = {};
+
+    for (int i = 0; i < len - 1; i++) {
+      for (int j = i + 1; j < len; j++) {
+        auto left = 0 - sortedNums[i] - sortedNums[j];
+
+        if (map.contains(left)) {
+          vector<int> v = {sortedNums[i], sortedNums[j], left};
+
+          res.insert(v);
+        }
+      }
+      map.insert(sortedNums[i]);
+    }
+
+    for (const auto& v : res) output.push_back(v);
+
+    return output;
+  }
+  static bool compareVectors(const vector<int>& v1, const vector<int>& v2) {
+    if (v1.size() != v2.size()) {
+      return false;
+    }
+
+    vector<int> sortedV1 = v1;
+    vector<int> sortedV2 = v2;
+    sort(sortedV1.begin(), sortedV1.end());
+    sort(sortedV2.begin(), sortedV2.end());
+
+    return sortedV1 == sortedV2;
+  }
+};
--- runtime-cc/src/registration.cc
+++ runtime-cc/src/registration.cc
@@ -3,6 +3,7 @@
 #include "../leetcode-cc/IntegerToRoman.hpp"
 #include "../leetcode-cc/LongestCommonPrefix.hpp"
 #include "../leetcode-cc/RomanToInteger.hpp"
+#include "../leetcode-cc/ThreeSum.hpp"
 
 const int registerAll(std::shared_ptr<Container> handle) {
   handle->registerProblem(
@@ -19,6 +20,11 @@ const int registerAll(std::shared_ptr<Container> handle) {
       []() -> ArcProblem { return std::make_shared<PLongestCommonPrefix>(); });
   handle->registerSolution(
       []() -> ArcSolution { return std::make_shared<SLonestCommonPrefix>(); });
+
+  handle->registerProblem(
+      []() -> ArcProblem { return std::make_shared<PThreeSum>(); });
+  handle->registerSolution(
+      []() -> ArcSolution { return std::make_shared<SThreeSum>(); });
 
   return 0;
 }
