690e61aa4901e30dbeec5ca98819ed67db4e4b40
SKTT1Ryze <ccc@neos.moe>
2023-11-13 11:17:27 +0900

    impl solution

--- leetcode-rs/src/solutions/substring_with_concatenation.rs
+++ leetcode-rs/src/solutions/substring_with_concatenation.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use super::Solution;
 
 pub struct SolutionImpl;
@@ -29,6 +31,12 @@ impl Solution for SolutionImpl {
                 vec!["bar".into(), "foo".into(), "the".into()],
                 vec![6, 9, 12],
             ),
+            (
+                "wordgoodgoodgoodbestword",
+                vec!["word".into(), "good".into(), "best".into(), "good".into()],
+                vec![8],
+            ),
+            ("a", vec!["a".into(), "a".into()], vec![]),
         ];
 
         for (s, words, expect) in testcases {
@@ -50,6 +58,50 @@ impl Solution for SolutionImpl {
 
 impl SolutionImpl {
     pub fn find_substring(s: String, words: Vec<String>) -> Vec<i32> {
-        todo!()
+        if words.is_empty() {
+            return vec![];
+        }
+        let word_len = words[0].len();
+        let len = word_len * words.len();
+        if s.len() < len {
+            return vec![];
+        }
+
+        let mut map: HashMap<String, usize> = HashMap::new();
+        for word in &words {
+            *map.entry(word.to_owned()).or_default() += 1;
+        }
+
+        let mut res = Vec::new();
+        let mut start = 0;
+
+        while start <= s.len().wrapping_sub(len) {
+            let mut map_cloned = map.clone();
+            let mut head = start;
+            let mut tail = head + word_len;
+
+            while tail <= s.len() {
+                if let Some(val) = map_cloned.get_mut(&s[head..tail]) {
+                    if *val == 1 {
+                        map_cloned.remove(&s[head..tail]);
+                    } else {
+                        *val -= 1;
+                    }
+                } else {
+                    break;
+                }
+
+                head += word_len;
+                tail += word_len;
+            }
+
+            if map_cloned.is_empty() {
+                res.push(start as i32);
+            }
+
+            start += 1;
+        }
+
+        res
     }
 }
