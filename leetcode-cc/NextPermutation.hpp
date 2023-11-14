e6ee1fedc9bf6a0f4dae8690a450da008028aae3
SKTT1Ryze <ccc@neos.moe>
2023-11-14 10:37:30 +0900

    save

--- leetcode-cc/NextPermutation.hpp
+++ leetcode-cc/NextPermutation.hpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include "problem.h"
 #include "solution.h"
 
@@ -22,6 +24,7 @@ class SNextPermutation : public ISolution {
         make_pair<vector<int>, vector<int>>({1, 2, 3}, {1, 3, 2}),
         make_pair<vector<int>, vector<int>>({3, 2, 1}, {1, 2, 3}),
         make_pair<vector<int>, vector<int>>({1, 1, 5}, {1, 5, 1}),
+        make_pair<vector<int>, vector<int>>({2, 3, 1}, {3, 1, 2}),
     };
 
     for (auto& [nums, expect] : testCases) {
@@ -35,5 +38,25 @@ class SNextPermutation : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  void nextPermutation(vector<int>& nums) const {}
+  void nextPermutation(vector<int>& nums) const {
+    if (nums.empty() || nums.size() == 1) return;
+    int i = nums.size() - 1;
+
+    while (i > 0) {
+      int j = i - 1;
+      while (j >= 0) {
+        if (nums[i] > nums[j]) {
+          int temp = nums.at(i);
+          nums.erase(nums.begin() + i);
+          nums.insert(nums.begin() + j, temp);
+          sort(nums.begin() + j + 1, nums.end());
+          return;
+        }
+        j--;
+      }
+      i--;
+    }
+
+    sort(nums.begin(), nums.end());
+  }
 };
