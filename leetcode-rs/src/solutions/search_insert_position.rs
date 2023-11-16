aed98c3644ab07f5b614ecc0f574a4f2222c617e
SKTT1Ryze <ccc@neos.moe>
2023-11-16 21:52:50 +0900

    impl solution Search Insert Position

--- leetcode-rs/src/solutions/search_insert_position.rs
+++ leetcode-rs/src/solutions/search_insert_position.rs
@@ -1,4 +1,5 @@
 use super::{test_helper, Solution};
+use std::cmp::Ordering;
 
 pub struct SolutionImpl;
 
@@ -18,8 +19,9 @@ impl Solution for SolutionImpl {
                 (vec![1, 3, 5, 6], 5),
                 (vec![1, 3, 5, 6], 2),
                 (vec![1, 3, 5, 6], 7),
+                (vec![1, 3, 5, 6], 0),
             ],
-            [2, 1, 4],
+            [2, 1, 4, 0],
             |(nums, target)| Self::search_insert(nums, target),
         )
     }
@@ -27,6 +29,26 @@ impl Solution for SolutionImpl {
 
 impl SolutionImpl {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        todo!()
+        let mut ans = 0;
+        let mut m = 0i32;
+        let mut n = nums.len() as i32 - 1;
+
+        while m <= n && m >= 0 && n >= 0 {
+            let k = (m + n) / 2;
+
+            match target.cmp(&nums[k as usize]) {
+                Ordering::Equal => return k,
+                Ordering::Greater => {
+                    ans = n + 1;
+                    m = k + 1;
+                }
+                Ordering::Less => {
+                    ans = m;
+                    n = k - 1;
+                }
+            }
+        }
+
+        ans
     }
 }
