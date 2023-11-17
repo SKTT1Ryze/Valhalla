d31c79571a3f332ce61c206a687fe46efc42595f
SKTT1Ryze <ccc@neos.moe>
2023-11-17 21:30:11 +0900

    impl Count and Say

--- leetcode-rs/src/solutions/count_and_say.rs
+++ leetcode-rs/src/solutions/count_and_say.rs
@@ -19,6 +19,33 @@ impl Solution for SolutionImpl {
 
 impl SolutionImpl {
     pub fn count_and_say(n: i32) -> String {
-        todo!()
+        Self::walk("1", n - 1)
+    }
+
+    fn walk(s: &str, n: i32) -> String {
+        if n > 0 {
+            let mut ans = "".to_string();
+            let mut stack = Vec::new();
+            for ch in s.chars() {
+                if !stack.is_empty() {
+                    let prev = stack[stack.len() - 1];
+                    if prev == ch {
+                        stack.push(ch);
+                    } else {
+                        ans.push_str(&format!("{}{prev}", stack.len()));
+                        stack.clear();
+                        stack.push(ch);
+                    }
+                } else {
+                    stack.push(ch);
+                }
+            }
+            if !stack.is_empty() {
+                ans.push_str(&format!("{}{}", stack.len(), stack[0]));
+            }
+            Self::walk(&ans, n - 1)
+        } else {
+            s.to_owned()
+        }
     }
 }
