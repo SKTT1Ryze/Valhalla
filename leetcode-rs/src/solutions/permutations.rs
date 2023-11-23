1a65415f4b2831058c817f2cfe0a5bb618be1cda
SKTT1Ryze <ccc@neos.moe>
2023-11-23 11:26:42 +0900

    impl permutations

--- leetcode-rs/src/solutions/permutations.rs
+++ leetcode-rs/src/solutions/permutations.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use super::{test_helper, Solution};
 
 pub struct SolutionImpl;
@@ -27,7 +29,24 @@ crate::derive_solution!(
 );
 
 impl SolutionImpl {
-    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        todo!()
+    pub fn permute(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut ans = HashSet::new();
+
+        Self::backtrack(&mut nums, 0, &mut ans);
+
+        ans.into_iter().collect()
+    }
+
+    fn backtrack(nums: &mut Vec<i32>, start: usize, ans: &mut HashSet<Vec<i32>>) {
+        ans.insert(nums.clone());
+        let n = nums.len();
+        if start < n - 1 {
+            Self::backtrack(nums, start + 1, ans);
+            for next in start + 1..n {
+                nums.swap(start, next);
+                Self::backtrack(nums, start + 1, ans);
+                nums.swap(start, next);
+            }
+        }
     }
 }
