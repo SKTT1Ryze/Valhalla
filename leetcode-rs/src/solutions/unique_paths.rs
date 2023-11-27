0bb090df692827ef4974f0774713dfa250c57145
SKTT1Ryze <ccc@neos.moe>
2023-11-27 10:03:06 +0900

    impl Unique Paths

--- leetcode-rs/src/solutions/unique_paths.rs
+++ leetcode-rs/src/solutions/unique_paths.rs
@@ -13,6 +13,24 @@ crate::derive_solution!(
 
 impl SolutionImpl {
     pub fn unique_paths(m: i32, n: i32) -> i32 {
-        todo!()
+        let m = m as usize;
+        let n = n as usize;
+        let mut dp = vec![vec![0; n]; m];
+
+        #[allow(clippy::needless_range_loop)]
+        for i in 0..m {
+            dp[i][0] = 1;
+        }
+        for i in 0..n {
+            dp[0][i] = 1;
+        }
+
+        for i in 1..m {
+            for j in 1..n {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        dp[m - 1][n - 1]
     }
 }
