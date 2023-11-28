fe184d5f755c9dac19e001cd3acb1755cf1449e2
SKTT1Ryze <ccc@neos.moe>
2023-11-28 10:51:59 +0900

    impl Climbing Stairs

--- leetcode-rs/src/solutions/climbing_stairs.rs
+++ leetcode-rs/src/solutions/climbing_stairs.rs
@@ -13,6 +13,19 @@ crate::derive_solution!(
 
 impl SolutionImpl {
     pub fn climb_stairs(n: i32) -> i32 {
-        todo!()
+        if n == 1 {
+            return 1;
+        }
+        let n = n as usize;
+        let mut dp = vec![0; n + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for i in 3..=n {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        dp[n]
     }
 }
