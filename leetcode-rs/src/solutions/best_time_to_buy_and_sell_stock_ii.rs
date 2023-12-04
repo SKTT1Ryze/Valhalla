2a8da227a18d68411d245204fc3dd187d8920020
SKTT1Ryze <ccc@neos.moe>
2023-12-04 11:08:05 +0900

    impl best-time-to-buy-and-sell-tock-ii

--- leetcode-rs/src/solutions/best_time_to_buy_and_sell_stock_ii.rs
+++ leetcode-rs/src/solutions/best_time_to_buy_and_sell_stock_ii.rs
@@ -17,6 +17,14 @@ crate::derive_solution!(
 
 impl SolutionImpl {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        todo!()
+        let mut profit = 0;
+
+        for i in 0..prices.len() - 1 {
+            if prices[i] < prices[i + 1] {
+                profit += prices[i + 1] - prices[i];
+            }
+        }
+
+        profit
     }
 }
