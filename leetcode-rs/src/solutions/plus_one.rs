2ebc9d9892563d592a3163c8b71d377dfa7dc265
SKTT1Ryze <ccc@neos.moe>
2023-12-19 22:48:23 +0900

    impl Plus One

--- leetcode-rs/src/solutions/plus_one.rs
+++ leetcode-rs/src/solutions/plus_one.rs
@@ -12,7 +12,20 @@ crate::derive_solution!(
 );
 
 impl SolutionImpl {
-    pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
-        todo!()
+    pub fn plus_one(mut digits: Vec<i32>) -> Vec<i32> {
+        let mut res = vec![];
+        let mut upgrade = 1;
+
+        while let Some(digit) = digits.pop() {
+            let sum = digit + upgrade;
+            upgrade = sum / 10;
+            res.push(sum % 10);
+        }
+
+        if upgrade > 0 {
+            res.push(upgrade);
+        }
+
+        res.into_iter().rev().collect()
     }
 }
