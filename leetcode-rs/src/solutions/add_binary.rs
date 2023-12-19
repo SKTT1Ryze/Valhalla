df6a42415e8fc72e334f293bb2740351edf66daa
SKTT1Ryze <ccc@neos.moe>
2023-12-19 23:16:35 +0900

    impl Add Binary

--- leetcode-rs/src/solutions/add_binary.rs
+++ leetcode-rs/src/solutions/add_binary.rs
@@ -12,7 +12,36 @@ crate::derive_solution!(
 );
 
 impl SolutionImpl {
-    pub fn add_binary(a: String, b: String) -> String {
-        todo!()
+    pub fn add_binary(mut a: String, mut b: String) -> String {
+        let mut res = Vec::<char>::new();
+        let mut upgrade = 0;
+
+        while !(a.is_empty() || b.is_empty()) {
+            let a = a.pop().unwrap() as u8 - 48;
+            let b = b.pop().unwrap() as u8 - 48;
+            let sum = a + b + upgrade;
+            upgrade = sum / 2;
+            res.push((sum % 2 + 48) as char);
+        }
+
+        while let Some(a) = a.pop() {
+            let a = a as u8 - 48;
+            let sum = a + upgrade;
+            upgrade = sum / 2;
+            res.push((sum % 2 + 48) as char);
+        }
+
+        while let Some(b) = b.pop() {
+            let b = b as u8 - 48;
+            let sum = b + upgrade;
+            upgrade = sum / 2;
+            res.push((sum % 2 + 48) as char);
+        }
+
+        if upgrade > 0 {
+            res.push('1');
+        }
+
+        res.into_iter().rev().collect()
     }
 }
