008278a6a468fde693f6a1a0c3f7cd40b6cb100c
SKTT1Ryze <ccc@neos.moe>
2023-12-22 11:23:52 +0900

    impl Permutation Sequence

--- leetcode-rs/src/solutions/permutation_sequence.rs
+++ leetcode-rs/src/solutions/permutation_sequence.rs
@@ -13,6 +13,39 @@ crate::derive_solution!(
 
 impl SolutionImpl {
     pub fn get_permutation(n: i32, k: i32) -> String {
-        todo!()
+        let mut init = vec![];
+        for i in 1..=n {
+            init.push(i);
+        }
+        for _ in 0..k - 1 {
+            Self::next_permutation(&mut init);
+        }
+        init.into_iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<_>>()
+            .join("")
+    }
+
+    fn next_permutation(nums: &mut [i32]) {
+        let mut breakpt = -1;
+        for i in (0..nums.len() - 1).rev() {
+            if nums[i] < nums[i + 1] {
+                breakpt = i as i32;
+                break;
+            }
+        }
+        if breakpt == -1 {
+            nums.reverse();
+        } else {
+            let breakpt = breakpt as usize;
+            for i in ((breakpt + 1)..nums.len()).rev() {
+                if nums[i] > nums[breakpt] {
+                    nums.swap(i, breakpt);
+                    break;
+                }
+            }
+
+            nums[breakpt + 1..].reverse();
+        }
     }
 }
