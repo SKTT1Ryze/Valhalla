4f32e500ae6de99dc08d4b48cefda6a73612f374
SKTT1Ryze <ccc@neos.moe>
2023-12-25 22:21:21 +0900

    impl Valid Number

--- leetcode-rs/src/solutions/valid_number.rs
+++ leetcode-rs/src/solutions/valid_number.rs
@@ -13,6 +13,49 @@ crate::derive_solution!(
 
 impl SolutionImpl {
     pub fn is_number(s: String) -> bool {
-        todo!()
+        if let Some(e) = s.chars().position(|ch| ch == 'e' || ch == 'E') {
+            Self::valid_digit(&s[..e]) && Self::valid_integer(&s[e + 1..])
+        } else {
+            Self::valid_digit(&s)
+        }
+    }
+
+    fn valid_digit(s: &str) -> bool {
+        let s: Vec<_> = s.chars().collect();
+        if s.is_empty() {
+            false
+        } else {
+            let dot = s.iter().position(|&ch| ch == '.');
+            if let Some(dot) = dot {
+                if dot == 0 {
+                    s.len() > 1 && s[1..].iter().all(char::is_ascii_digit)
+                } else if dot < s.len() - 1 {
+                    Self::valid_head(&s)
+                        && s[1..dot].iter().all(char::is_ascii_digit)
+                        && s[dot + 1..].iter().all(char::is_ascii_digit)
+                } else {
+                    Self::valid_head(&s)
+                        && s[1..dot].iter().all(char::is_ascii_digit)
+                }
+            } else {
+                Self::valid_head(&s) && s[1..].iter().all(char::is_ascii_digit)
+            }
+        }
+    }
+
+    fn valid_integer(s: &str) -> bool {
+        let s: Vec<_> = s.chars().collect();
+        if s.is_empty() {
+            false
+        } else {
+            Self::valid_head(&s) && s[1..].iter().all(char::is_ascii_digit)
+        }
+    }
+
+    fn valid_head(s: &[char]) -> bool {
+        (s[0] == '+' || s[0] == '-')
+            && s.len() > 1
+            && (s.len() == 2 && s[1] != '.' || s.len() > 2)
+            || s[0].is_ascii_digit()
     }
 }
