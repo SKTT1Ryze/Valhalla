3455199bc7761ddc353312582c37f44b9cdbc4b6
SKTT1Ryze <ccc@neos.moe>
2023-12-27 12:56:08 +0900

    impl Subsets

--- leetcode-rs/src/solutions/subsets.rs
+++ leetcode-rs/src/solutions/subsets.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use super::{test_helper, Solution};
 
 pub struct SolutionImpl;
@@ -11,20 +12,38 @@ crate::derive_solution!(
         vec![
             vec![],
             vec![1],
-            vec![2],
             vec![1, 2],
-            vec![3],
+            vec![1, 2, 3],
             vec![1, 3],
+            vec![2],
             vec![2, 3],
-            vec![1, 2, 3]
+            vec![3]
         ],
         vec![vec![], vec![0]]
     ],
-    Self::subsets
+    |nums| {
+        let mut res = Self::subsets(nums);
+        res.sort();
+        res
+    }
 );
 
 impl SolutionImpl {
-    pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        todo!()
+    pub fn subsets(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut ans = HashSet::new();
+        Self::backtracking(&mut ans, &mut nums);
+        ans.insert(vec![]);
+        ans.into_iter().collect()
+    }
+
+    fn backtracking(ans: &mut HashSet<Vec<i32>>, current: &mut Vec<i32>) {
+        if !current.is_empty() {
+            ans.insert(current.clone());
+        }
+        for i in 0..current.len() {
+            let e = current.remove(i);
+            Self::backtracking(ans, current);
+            current.insert(i, e);
+        }
     }
 }
