c77cc71e896e5dea11e267b00a28d363532a2c00
Chunchi Che <ccc@neos.moe>
2023-12-29 18:31:53 +0900

    Merge sorted array (#109)
    * add RemoveSortedArray
    
    * impl RemoveSortedArray

---
+++ leetcode-cc/MergeSortedArray.hpp
@@ -0,0 +1,54 @@
+#include <tuple>
+
+#include "problem.h"
+#include "solution.h"
+#include "util.h"
+
+using namespace std;
+
+IMPLEMENT_PROBLEM_CLASS(
+    PMergeSortedArray, 88, DIFFI_EASY, TOPIC_ALGORITHMS, "Merge Sorted Array",
+    "Merge nums1 and nums2 into a single array sorted in non-decreasing order.",
+    {"Array"});
+
+class SMergeSortedArray : public ISolution {
+ public:
+  size_t problemId() const override { return 88; }
+  string name() const override { return "Solution for Merge Sorted Array"; }
+  string location() const override { return __FILE_NAME__; }
+  int test() const override {
+    vector<tuple<vector<int>, int, vector<int>, int, vector<int>>> testCases = {
+        make_tuple(vector<int>({1, 2, 3, 0, 0, 0}), 3, vector<int>({2, 5, 6}),
+                   3, vector<int>({1, 2, 2, 3, 5, 6})),
+        make_tuple(vector<int>({1}), 1, vector<int>({}), 0, vector<int>({1}))};
+
+    for (auto& [nums1, m, nums2, n, expect] : testCases) {
+      this->merge(nums1, m, nums2, n);
+      if (!compareVectors(nums1, expect)) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+  int benchmark() const override { return 0; }
+
+ private:
+  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) const {
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+
+    // from back to front
+    while (i >= 0 && j >= 0) {
+      if (nums1[i] > nums2[j]) {
+        nums1[k--] = nums1[i--];
+      } else {
+        nums1[k--] = nums2[j--];
+      }
+    }
+
+    while (j >= 0) {
+      nums1[k--] = nums2[j--];
+    }
+  }
+};
