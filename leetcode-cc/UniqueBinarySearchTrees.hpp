7daf30a14f111dcde8218010bee040073b4f0447
SKTT1Ryze <ccc@neos.moe>
2024-01-08 15:34:00 +0900

    impl Unique Binary Search Trees

--- leetcode-cc/UniqueBinarySearchTrees.hpp
+++ leetcode-cc/UniqueBinarySearchTrees.hpp
@@ -25,5 +25,18 @@ class SUqBinarySearchTrees : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  int numTrees(int n) const {}
+  int numTrees(int n) const {
+    vector<int> dp(n + 1, 0);
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        dp[i] += dp[j - 1] * dp[i - j];
+      }
+    }
+
+    return dp[n];
+  }
 };
