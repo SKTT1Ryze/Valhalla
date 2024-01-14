0503768634b5f8a33a162585748e075c60a64874
SKTT1Ryze <ccc@neos.moe>
2024-01-14 13:23:01 +0900

    impl Determin If Two Strings Are Close

--- leetcode-cc/DetIfTwoStrsAreClose.hpp
+++ leetcode-cc/DetIfTwoStrsAreClose.hpp
@@ -1,4 +1,5 @@
 #include <tuple>
+#include <unordered_map>
 
 #include "TestHelper.h"
 #include "problem.h"
@@ -19,13 +20,46 @@ class SDetIfTwoStrsAreClose : public ISolution {
   string location() const override { return __FILE_NAME__; }
   int test() const override {
     return testHelper<tuple<string, string>, bool>(
-        {{"abc", "bca"}, {"a", "aa"}, {"cabbba", "abbccc"}},
-        {true, false, true}, [this](auto input) {
+        {{"abc", "bca"},
+         {"a", "aa"},
+         {"cabbba", "abbccc"},
+         {"aabbcccddd", "abccccdddd"}},
+        {true, false, true, false}, [this](auto input) {
           return this->closeStrings(get<0>(input), get<1>(input));
         });
   };
   int benchmark() const override { return 0; }
 
  private:
-  bool closeStrings(string word1, string word2) const {}
+  bool closeStrings(string word1, string word2) const {
+    if (word1.size() != word2.size()) return false;
+    unordered_map<char, int> map1 = {};
+    unordered_map<char, int> map2 = {};
+
+    for (const auto& ch : word1) {
+      map1[ch]++;
+    }
+
+    for (const auto& ch : word2) {
+      map2[ch]++;
+    }
+
+    if (map1.size() != map2.size()) return false;
+
+    vector<int> count1, count2;
+
+    for (const auto& [key, value] : map1) {
+      count1.push_back(value);
+      if (map2.contains(key)) {
+        count2.push_back(map2[key]);
+      } else {
+        return false;
+      }
+    }
+
+    sort(count1.begin(), count1.end());
+    sort(count2.begin(), count2.end());
+
+    return count1 == count2;
+  }
 };
