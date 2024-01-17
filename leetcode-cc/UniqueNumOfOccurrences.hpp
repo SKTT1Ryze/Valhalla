83b5cf1459840ed22d893ffce12db6daf8ed79ee
SKTT1Ryze <ccc@neos.moe>
2024-01-17 10:45:44 +0900

    impl unique number of occurrences

--- leetcode-cc/UniqueNumOfOccurrences.hpp
+++ leetcode-cc/UniqueNumOfOccurrences.hpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <unordered_map>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -9,7 +12,7 @@ IMPLEMENT_PROBLEM_CLASS(
     "Unique Number of Occurrences",
     "Given an array of integers arr, return true if the number of occurrences "
     "of each value in the array is unique or false otherwise.",
-    {""});
+    {"Hash Table"});
 
 class SUniqueNumOfOccurrences : public ISolution {
  public:
@@ -27,5 +30,23 @@ class SUniqueNumOfOccurrences : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  bool uniqueOccurrences(vector<int>& arr) const {}
+  bool uniqueOccurrences(vector<int>& arr) const {
+    unordered_map<int, int> hash = {};
+    for (const auto& num : arr) {
+      hash[num]++;
+    }
+
+    vector<int> values(hash.size());
+    transform(hash.begin(), hash.end(), values.begin(),
+              [](auto pair) { return pair.second; });
+
+    if (values.size() == 1) return true;
+
+    sort(values.begin(), values.end());
+    for (int i = 0; i < values.size() - 1; i++) {
+      if (values[i] == values[i + 1]) return false;
+    }
+
+    return true;
+  }
 };
