05b6af06a02aa0598110ea255adaa661a248d3a4
SKTT1Ryze <ccc@neos.moe>
2024-01-18 11:26:08 +0900

    impl Binary Tree Zigzag Level Order Traversal

--- leetcode-cc/BTreeZigzagLevelOrderTraversal.hpp
+++ leetcode-cc/BTreeZigzagLevelOrderTraversal.hpp
@@ -1,3 +1,5 @@
+#include <queue>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -22,12 +24,51 @@ class SBTreeZigzagLevelOrderTraversal : public ISolution {
   string location() const override { return __FILE_NAME__; }
   int test() const override {
     return testHelperBinaryTreeV2<vector<vector<int>>>(
-        {"[3,9,20,null,null,15,7]", "[1]", "[]"},
-        {{{3}, {20, 9}, {15, 7}}, {{1}}, {}},
+        {"[3,9,20,null,null,15,7]", "[1]", "[]", "[1,2,3,4,null,null,5]"},
+        {{{3}, {20, 9}, {15, 7}}, {{1}}, {}, {{1}, {3, 2}, {4, 5}}},
         [this](auto root) { return this->zigzagLevelOrder(root); });
   };
   int benchmark() const override { return 0; }
 
  private:
-  vector<vector<int>> zigzagLevelOrder(TreeNode* root) const {}
+  vector<vector<int>> zigzagLevelOrder(TreeNode* root) const {
+    if (root == nullptr) return {};
+    vector<vector<int>> result;
+    if (!root) {
+      return result;
+    }
+
+    queue<TreeNode*> q;
+    q.push(root);
+    bool reverseOrder = false;
+
+    while (!q.empty()) {
+      int size = q.size();
+      vector<int> level;
+
+      for (int i = 0; i < size; ++i) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        if (reverseOrder) {
+          level.insert(level.begin(), node->val);
+        } else {
+          level.push_back(node->val);
+        }
+
+        if (node->left) {
+          q.push(node->left);
+        }
+
+        if (node->right) {
+          q.push(node->right);
+        }
+      }
+
+      result.push_back(level);
+      reverseOrder = !reverseOrder;
+    }
+
+    return result;
+  }
 };
