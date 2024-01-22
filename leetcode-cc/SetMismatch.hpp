c3f543375bb01b357abf85f28b6c8a3b8980236f
SKTT1Ryze <ccc@neos.moe>
2024-01-22 23:52:57 +0900

    impl Set Mismatch

--- leetcode-cc/SetMismatch.hpp
+++ leetcode-cc/SetMismatch.hpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <unordered_set>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -5,7 +8,7 @@
 using namespace std;
 
 IMPLEMENT_PROBLEM_CLASS(PSetMisMatch, 645, DIFFI_EASY, TOPIC_ALGORITHMS,
-                        "Set Mismatch", "", {""});
+                        "Set Mismatch", "", {"Math"});
 
 class SSetMisMatch : public ISolution {
  public:
@@ -22,5 +25,23 @@ class SSetMisMatch : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  vector<int> findErrorNums(vector<int>& nums) const {}
+  vector<int> findErrorNums(vector<int>& nums) const {
+    int dup = -1, missing = -1;
+
+    for (int i = 1; i <= nums.size(); i++) {
+      int count = 0;
+      for (int j = 0; j < nums.size(); j++) {
+        if (nums[j] == i) {
+          count++;
+        }
+      }
+      if (count == 2) {
+        dup = i;
+      } else if (count == 0) {
+        missing = i;
+      }
+    }
+
+    return {dup, missing};
+  }
 };
