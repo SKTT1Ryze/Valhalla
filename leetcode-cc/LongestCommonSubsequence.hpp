0894cec2834bb8de24c59f9099c54991453bae15
SKTT1Ryze <ccc@neos.moe>
2024-01-25 11:15:58 +0900

    impl Longest Common Subsequence

--- leetcode-cc/LongestCommonSubsequence.hpp
+++ leetcode-cc/LongestCommonSubsequence.hpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <utility>
 
 #include "TestHelper.h"
@@ -6,9 +7,12 @@
 
 using namespace std;
 
-IMPLEMENT_PROBLEM_CLASS(PLongestCommonSubsequence, 1143, DIFFI_MEDIUM,
-                        TOPIC_ALGORITHMS, "Longest Common Subsequence", "",
-                        {""});
+IMPLEMENT_PROBLEM_CLASS(
+    PLongestCommonSubsequence, 1143, DIFFI_MEDIUM, TOPIC_ALGORITHMS,
+    "Longest Common Subsequence",
+    "Given two strings text1 and text2, return the length of their longest "
+    "common subsequence. If there is no common subsequence, return 0.",
+    {""});
 
 class SLongestCommonSubsequence : public ISolution {
  public:
@@ -27,5 +31,23 @@ class SLongestCommonSubsequence : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  int longestCommonSubsequence(string text1, string text2) const {}
+  int longestCommonSubsequence(string text1, string text2) const {
+    int m = text1.size();
+    int n = text2.size();
+
+    vector<vector<int>> dp(m + 1, vector(n + 1, 0));
+    dp[0][0] = 0;
+
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (text1[i - 1] == text2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1] + 1;
+        } else {
+          dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+        }
+      }
+    }
+
+    return dp[m][n];
+  }
 };
