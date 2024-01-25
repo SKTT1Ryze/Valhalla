dd9c751633687094ff92dbd2019785de331ab460
Chunchi Che <ccc@neos.moe>
2024-01-25 11:54:32 +0900

    Maximum depth of binary tree (#144)
    * add Max Depth of Binary Tree
    
    * impl Max Depth of Binary Tree

---
+++ leetcode-cc/MaxDepthOfBTree.hpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+
+#include "TestHelper.h"
+#include "problem.h"
+#include "solution.h"
+
+using namespace std;
+
+IMPLEMENT_PROBLEM_CLASS(
+    PMaxDepthOfBTree, 104, DIFFI_EASY, TOPIC_ALGORITHMS,
+    "Maximum Depth of Binary Tree",
+    "Given the root of a binary tree, return its maximum depth.A binary tree's "
+    "maximum depth is the number of nodes along the longest path from the root "
+    "node down to the farthest leaf node.",
+    {"Binary Tree"});
+
+class SMaxDepthOfBTree : public ISolution {
+ public:
+  size_t problemId() const override { return 104; }
+  string name() const override {
+    return ("Solution for " + string("Maximum Depth of Binary Tree"));
+  }
+  string location() const override { return __FILE_NAME__; }
+  int test() const override {
+    return testHelperBinaryTreeV2<int>(
+        {"[3,9,20,null,null,15,7]", "[1,null,2]"}, {3, 2},
+        [this](auto root) { return this->maxDepth(root); });
+  }
+  int benchmark() const override { return 0; }
+
+ private:
+  int maxDepth(TreeNode* root) const {
+    if (root == nullptr) return 0;
+    return this->walk(root, 1);
+  }
+
+  int walk(TreeNode* root, int current) const {
+    if (!root->left && !root->right) {
+      return current;
+    } else if (!root->left) {
+      return this->walk(root->right, current + 1);
+    } else if (!root->right) {
+      return this->walk(root->left, current + 1);
+    } else {
+      return max(this->walk(root->left, current + 1),
+                 this->walk(root->right, current + 1));
+    }
+  }
+};
