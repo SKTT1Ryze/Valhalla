8342c60119ba85749fb21efc08cc5dd9272589f6
SKTT1Ryze <ccc@neos.moe>
2024-01-25 11:22:04 +0900

    fix

--- leetcode-cc/LongestCommonSubsequence.hpp
+++ leetcode-cc/LongestCommonSubsequence.hpp
@@ -12,7 +12,7 @@ IMPLEMENT_PROBLEM_CLASS(
     "Longest Common Subsequence",
     "Given two strings text1 and text2, return the length of their longest "
     "common subsequence. If there is no common subsequence, return 0.",
-    {""});
+    {"DP"});
 
 class SLongestCommonSubsequence : public ISolution {
  public:
