ef0be988be06cfabd1f3f690456218de8b171b06
SKTT1Ryze <ccc@neos.moe>
2024-01-25 23:02:28 +0900

    impl Construct Binary Tree From Preorder and Inorder Traversal

--- leetcode-cc/ConstructBTreePreorderInorder.hpp
+++ leetcode-cc/ConstructBTreePreorderInorder.hpp
@@ -35,5 +35,37 @@ class SConstructBTreePreorderInorder : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) const {}
+  TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) const {
+    unordered_map<int, int> inorder_map;
+    for (int i = 0; i < inorder.size(); i++) {
+      inorder_map[inorder[i]] = i;
+    }
+    return buildTreeHelper(preorder, 0, preorder.size(), inorder, 0,
+                           inorder.size(), inorder_map);
+  }
+
+  TreeNode* buildTreeHelper(const vector<int>& preorder, int pre_start,
+                            int pre_end, const vector<int>& inorder,
+                            int in_start, int in_end,
+                            unordered_map<int, int>& inorder_map) const {
+    if (pre_start == pre_end) {
+      return nullptr;
+    }
+
+    int root_val = preorder[pre_start];
+    TreeNode* root = new TreeNode(root_val);
+
+    int root_index_inorder = inorder_map[root_val];
+    int left_tree_size = root_index_inorder - in_start;
+
+    root->left =
+        buildTreeHelper(preorder, pre_start + 1, pre_start + 1 + left_tree_size,
+                        inorder, in_start, root_index_inorder, inorder_map);
+
+    root->right =
+        buildTreeHelper(preorder, pre_start + 1 + left_tree_size, pre_end,
+                        inorder, root_index_inorder + 1, in_end, inorder_map);
+
+    return root;
+  }
 };
