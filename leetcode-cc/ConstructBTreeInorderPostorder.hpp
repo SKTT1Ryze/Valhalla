cbfb617da289de1a15c0a7fcf668c4ddbdd9a50a
SKTT1Ryze <ccc@neos.moe>
2024-01-28 16:15:05 +0900

    impl Construct Binary Tree From Inorder And Postorder Travesal

--- leetcode-cc/ConstructBTreeInorderPostorder.hpp
+++ leetcode-cc/ConstructBTreeInorderPostorder.hpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include "BinaryTree.h"
 #include "problem.h"
 #include "solution.h"
@@ -34,5 +36,33 @@ class SConstructBTreeInorderPostorder : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) const {}
+  TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) const {
+    unordered_map<int, int> inorder_map;
+    for (int i = 0; i < inorder.size(); i++) inorder_map[inorder[i]] = i;
+
+    return this->buildTreeHelper(inorder, postorder, 0, inorder.size() - 1, 0,
+                                 postorder.size() - 1, inorder_map);
+  }
+
+  TreeNode* buildTreeHelper(vector<int>& inorder, vector<int>& postorder,
+                            int in_start, int in_end, int post_start,
+                            int post_end,
+                            unordered_map<int, int>& inorder_map) const {
+    if (in_start > in_end || post_start > post_end) return nullptr;
+
+    int root_val = postorder[post_end];
+    TreeNode* root = new TreeNode(root_val);
+
+    int root_index = inorder_map[root_val];
+    int left_subtree_size = root_index - in_start;
+
+    root->left = this->buildTreeHelper(
+        inorder, postorder, in_start, root_index - 1, post_start,
+        post_start + left_subtree_size - 1, inorder_map);
+    root->right = this->buildTreeHelper(inorder, postorder, root_index + 1,
+                                        in_end, post_start + left_subtree_size,
+                                        post_end - 1, inorder_map);
+
+    return root;
+  }
 };
