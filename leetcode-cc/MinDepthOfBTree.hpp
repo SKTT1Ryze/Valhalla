6e7fc6b47fb116d955d8cddd180c88b34a2d2e77
Chunchi Che <ccc@neos.moe>
2024-01-30 22:37:25 +0900

    add mininum depth of binary tree (#153)
    * add mininum depth of binary tree
    
    * impl mininum depth of binary tree

---
+++ leetcode-cc/MinDepthOfBTree.hpp
@@ -0,0 +1,36 @@
+#include "TestHelper.h"
+#include "problem.h"
+#include "solution.h"
+
+using namespace std;
+
+IMPLEMENT_PROBLEM_CLASS(PMinDepthOfBTree, 111, DIFFI_EASY, TOPIC_ALGORITHMS,
+                        "Minimum Depth of Binary Tree",
+                        "Given a binary tree, find its minimum depth.",
+                        {"Binary Tree"});
+
+class SMinDepthOfBTree : public ISolution {
+ public:
+  size_t problemId() const override { return 111; }
+  string name() const override {
+    return ("Solution for " + string("Minimum Depth of Binary Tree"));
+  }
+  string location() const override { return __FILE_NAME__; }
+  int test() const override {
+    return testHelperBinaryTreeV2<int>(
+        {"[3,9,20,null,null,15,7]", "[2,null,3,null,4,null,5,null,6]"}, {2, 5},
+        [this](auto root) { return this->minDepth(root); });
+  };
+  int benchmark() const override { return 0; }
+
+ private:
+  int minDepth(TreeNode* root) const {
+    if (root == nullptr) return 0;
+    if (root->left == nullptr)
+      return this->minDepth(root->right) + 1;
+    else if (root->right == nullptr)
+      return this->minDepth(root->left) + 1;
+    else
+      return min(this->minDepth(root->left), this->minDepth(root->right)) + 1;
+  }
+};
