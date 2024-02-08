c67fbc6b5d65b4e6a412a9c05ae5decf15a836db
SKTT1Ryze <ccc@neos.moe>
2024-02-08 14:35:47 +0900

    impl longest consecutive sequence

--- leetcode-cc/LongestConsecutiveSeq.hpp
+++ leetcode-cc/LongestConsecutiveSeq.hpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -19,11 +21,39 @@ class SLongestConsecutiveSeq : public ISolution {
   string location() const override { return __FILE_NAME__; }
   int test() const override {
     return testHelper<vector<int>, int>(
-        {{100, 4, 200, 1, 3, 2}, {0, 3, 7, 2, 5, 8, 4, 6, 0, 1}}, {4, 9},
+        {{100, 4, 200, 1, 3, 2}, {0, 3, 7, 2, 5, 8, 4, 6, 0, 1}, {1, 2, 0, 1}},
+        {4, 9, 3},
         [this](auto input) { return this->longestConsecutive(input); });
   };
   int benchmark() const override { return 0; }
 
  private:
-  int longestConsecutive(vector<int>& nums) const {}
+  int longestConsecutive(vector<int>& nums) const {
+    int n = nums.size();
+    if (n == 0 || n == 1) return n;
+
+    int ans = 0;
+    sort(nums.begin(), nums.end());
+
+    int start = 0;
+    while (start < n - 1) {
+      int end = start + 1;
+      int len = 1;
+      while (end < n) {
+        if (nums[end] - nums[start] == len) {
+          len++;
+          end++;
+        } else if (nums[end] == nums[end - 1]) {
+          end++;
+        } else {
+          break;
+        }
+      }
+
+      ans = max(ans, len);
+      start = end;
+    }
+
+    return ans;
+  }
 };
