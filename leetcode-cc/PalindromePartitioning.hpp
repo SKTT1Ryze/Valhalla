891e4d59435dea9e4dc5a89e13cd2738b16cfe71
SKTT1Ryze <ccc@neos.moe>
2024-02-18 13:42:31 +0900

    impl palindrome partitioning

--- leetcode-cc/PalindromePartitioning.hpp
+++ leetcode-cc/PalindromePartitioning.hpp
@@ -9,7 +9,7 @@ IMPLEMENT_PROBLEM_CLASS(
     "Palindrome Partitioning",
     "Given a string s, partition s such that every substring of the partition "
     "is a palindrome. Return all possible palindrome partitioning of s.",
-    {""});
+    {"DP / Backtracking"});
 
 class SPalindromePartitioning : public ISolution {
  public:
@@ -26,5 +26,37 @@ class SPalindromePartitioning : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  vector<vector<string>> partition(string s) const {}
+  vector<vector<string>> partition(string s) const {
+    vector<string> current = {};
+    vector<vector<string>> paths = {};
+
+    this->backtracking(s, 0, current, paths);
+
+    return paths;
+  }
+
+  void backtracking(string& s, int start, vector<string>& current,
+                    vector<vector<string>>& paths) const {
+    if (start == s.size()) {
+      paths.push_back(current);
+    } else {
+      for (int i = start; i < s.size(); i++) {
+        if (this->isPalindrome(s, start, i)) {
+          auto substr = s.substr(start, i - start + 1);
+          current.push_back(substr);
+          this->backtracking(s, i + 1, current, paths);
+          current.pop_back();
+        }
+      }
+    }
+  }
+
+  bool isPalindrome(string& s, int start, int end) const {
+    while (start < end) {
+      if (s[start] != s[end]) return false;
+      ++start;
+      --end;
+    }
+    return true;
+  }
 };
