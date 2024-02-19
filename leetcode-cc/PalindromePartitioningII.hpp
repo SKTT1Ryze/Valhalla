11bba8195ccc57a896a76ec2d70b7fcac4265d1e
SKTT1Ryze <ccc@neos.moe>
2024-02-19 14:17:48 +0900

    impl palindrome partitioningII

--- leetcode-cc/PalindromePartitioningII.hpp
+++ leetcode-cc/PalindromePartitioningII.hpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <climits>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -10,7 +13,7 @@ IMPLEMENT_PROBLEM_CLASS(
     "Given a string s, partition s such that every substring of the partition "
     "is a palindrome. Return the minimum cuts needed for a palindrome "
     "partitioning of s.",
-    {""});
+    {"DP"});
 
 class SPalindromePartitioningII : public ISolution {
  public:
@@ -27,5 +30,34 @@ class SPalindromePartitioningII : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  int minCut(string s) const {}
+  int minCut(string s) const {
+    int n = s.size();
+    vector<int> dp(n + 1, 0);
+    vector<vector<bool>> isPalindrome(n, vector<bool>(n, false));
+
+    // Initialize dp
+    for (int i = 0; i <= n; i++) {
+      dp[i] = n - i - 1;
+    }
+
+    // Pre-calculate isPalindrome
+    for (int i = n - 1; i >= 0; i--) {
+      for (int j = i; j < n; j++) {
+        if (s[i] == s[j] && (j - i < 2 || isPalindrome[i + 1][j - 1])) {
+          isPalindrome[i][j] = true;
+        }
+      }
+    }
+
+    // DP
+    for (int i = n - 1; i >= 0; i--) {
+      for (int j = i; j < n; j++) {
+        if (isPalindrome[i][j]) {
+          dp[i] = min(dp[i], dp[j + 1] + 1);
+        }
+      }
+    }
+
+    return dp[0];
+  }
 };
