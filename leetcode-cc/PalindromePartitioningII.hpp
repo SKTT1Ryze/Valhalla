8751b916f667c2035be17d6ea882a5da1d68dab3
SKTT1Ryze <ccc@neos.moe>
2024-02-19 13:56:51 +0900

    add palindrome partitioningII

---
+++ leetcode-cc/PalindromePartitioningII.hpp
@@ -0,0 +1,31 @@
+#include "TestHelper.h"
+#include "problem.h"
+#include "solution.h"
+
+using namespace std;
+
+IMPLEMENT_PROBLEM_CLASS(
+    PPalindromePartitioningII, 132, DIFFI_HARD, TOPIC_ALGORITHMS,
+    "Palindrome Partitioning II",
+    "Given a string s, partition s such that every substring of the partition "
+    "is a palindrome. Return the minimum cuts needed for a palindrome "
+    "partitioning of s.",
+    {""});
+
+class SPalindromePartitioningII : public ISolution {
+ public:
+  size_t problemId() const override { return 132; }
+  string name() const override {
+    return ("Solution for " + string("Palindrome Partitioning II"));
+  }
+  string location() const override { return __FILE_NAME__; }
+  int test() const override {
+    return testHelper<string, int>(
+        {"aab", "a", "ab"}, {1, 0, 1},
+        [this](auto input) { return this->minCut(input); });
+  };
+  int benchmark() const override { return 0; }
+
+ private:
+  int minCut(string s) const {}
+};
