bb49d4e453d798e4b86b0368974cac9d2e1759bb
SKTT1Ryze <ccc@neos.moe>
2024-02-23 13:59:54 +0900

    impl word break

--- leetcode-cc/WordBreak.hpp
+++ leetcode-cc/WordBreak.hpp
@@ -1,3 +1,4 @@
+#include <unordered_set>
 #include <utility>
 
 #include "TestHelper.h"
@@ -11,7 +12,7 @@ IMPLEMENT_PROBLEM_CLASS(
     "Given a string s and a dictionary of strings wordDict, return true if s "
     "can be segmented into a space-separated sequence of one or more "
     "dictionary words.",
-    {""});
+    {"DP / BFS"});
 
 class SWordBreak : public ISolution {
  public:
@@ -32,5 +33,21 @@ class SWordBreak : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  bool wordBreak(string s, vector<string>& wordDict) const {}
+  bool wordBreak(string s, vector<string>& wordDict) const {
+    int n = s.size();
+    unordered_set<string> dict(wordDict.begin(), wordDict.end());
+    vector<bool> dp(n + 1, false);
+    dp[0] = true;
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < i; j++) {
+        if (dp[j] && dict.contains(s.substr(j, i - j))) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+
+    return dp[n];
+  }
 };
