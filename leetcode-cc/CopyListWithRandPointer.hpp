145ba8b0b9cf5b8696473a0bbb6c8a3a62a37dc3
SKTT1Ryze <ccc@neos.moe>
2024-03-03 11:24:34 +0800

    impl copy list with random pointer

--- leetcode-cc/CopyListWithRandPointer.hpp
+++ leetcode-cc/CopyListWithRandPointer.hpp
@@ -1,5 +1,6 @@
 #include "problem.h"
 #include "solution.h"
+#include "unordered_map"
 
 using namespace std;
 
@@ -31,5 +32,31 @@ class SCopyListWithRandPointer : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  RandNode* copyRandomList(RandNode* head) {}
+  RandNode* copyRandomList(RandNode* head) {
+    auto newHead = new RandNode(0);
+    auto p1 = head;
+    auto p2 = newHead;
+    unordered_map<RandNode*, RandNode*> map = {};
+
+    while (p1) {
+      auto node = new RandNode(p1->val);
+      p2->next = node;
+      p2 = p2->next;
+      map.insert({p1, p2});
+      p1 = p1->next;
+    }
+
+    p1 = head;
+    p2 = newHead->next;
+
+    while (p1) {
+      auto rand = map[p1->random];
+      p2->random = rand;
+
+      p1 = p1->next;
+      p2 = p2->next;
+    }
+
+    return newHead->next;
+  }
 };
