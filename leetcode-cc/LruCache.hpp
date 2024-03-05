ad9cefb45ac8c2e962fd5f377ebd6a6f75b586ba
SKTT1Ryze <ccc@neos.moe>
2024-03-05 14:15:55 +0800

    impl lru cache

--- leetcode-cc/LruCache.hpp
+++ leetcode-cc/LruCache.hpp
@@ -1,3 +1,5 @@
+#include <unordered_map>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -8,7 +10,7 @@ IMPLEMENT_PROBLEM_CLASS(PLRUCache, 146, DIFFI_MEDIUM, TOPIC_ALGORITHMS,
                         "LRU Cache",
                         "Design a data structure that follows the constraints "
                         "of a Least Recently Used (LRU) cache.",
-                        {"LRU"});
+                        {"Hash Table & Double Linked List"});
 
 class SLRUCache : public ISolution {
  public:
@@ -21,11 +23,79 @@ class SLRUCache : public ISolution {
   int benchmark() const override { return 0; }
 };
 
+struct DListNode {
+  int key;
+  int val;
+  DListNode* prev;
+  DListNode* next;
+  DListNode() : key(-1), val(-1), prev(nullptr), next(nullptr) {}
+  DListNode(int key, int val)
+      : key(key), val(val), prev(nullptr), next(nullptr) {}
+};
+
 class LRUCache {
  public:
-  LRUCache(int capacity) {}
+  LRUCache(int capacity) {
+    this->capacity = capacity;
+    this->head = new DListNode();
+    this->tail = new DListNode();
+    this->head->next = tail;
+    this->tail->prev = head;
+  }
 
-  int get(int key) {}
+  int get(int key) {
+    if (this->inner.contains(key)) {
+      auto node = this->inner[key];
+      this->moveToHead(node);
+      return node->val;
+    } else {
+      return -1;
+    }
+  }
 
-  void put(int key, int value) {}
+  void put(int key, int value) {
+    if (this->inner.contains(key)) {
+      auto node = this->inner[key];
+      node->val = value;
+      this->moveToHead(node);
+    } else {
+      if (this->inner.size() >= capacity) {
+        auto tail = this->removeTail();
+        this->inner.erase(tail->key);
+        delete tail;
+      }
+      auto node = new DListNode(key, value);
+      this->inner.insert({key, node});
+      this->addNodeToHead(node);
+    }
+  }
+
+ private:
+  unordered_map<int, DListNode*> inner = {};
+  DListNode* head;
+  DListNode* tail;
+  int capacity;
+
+  static void removeNode(DListNode* node) {
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+  }
+
+  void addNodeToHead(DListNode* node) {
+    node->next = this->head->next;
+    this->head->next->prev = node;
+    this->head->next = node;
+    node->prev = this->head;
+  }
+
+  void moveToHead(DListNode* node) {
+    removeNode(node);
+    this->addNodeToHead(node);
+  }
+
+  DListNode* removeTail() {
+    auto node = tail->prev;
+    removeNode(node);
+    return node;
+  }
 };
