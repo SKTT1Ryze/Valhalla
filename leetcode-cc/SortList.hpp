1a5278091dd3c3b0c02fb7243a7556fca6c11b63
SKTT1Ryze <ccc@neos.moe>
2024-03-06 10:41:10 +0800

    impl sort list

--- leetcode-cc/SortList.hpp
+++ leetcode-cc/SortList.hpp
@@ -25,5 +25,40 @@ class SSortList : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  ListNode* sortList(ListNode* head) const {}
+  ListNode* sortList(ListNode* head) const {
+    if (!head || !head->next) return head;
+    auto middle = findMiddle(head);
+    auto left = this->sortList(head);
+    auto right = this->sortList(middle);
+    return merge(left, right);
+  }
+
+  static ListNode* findMiddle(ListNode* head) {
+    if (!head || !head->next) return head;
+    auto slow = head;
+    auto fast = head->next;
+
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+
+    auto middle = slow->next;
+    slow->next = nullptr;
+
+    return middle;
+  }
+
+  static ListNode* merge(ListNode* l1, ListNode* l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+
+    if (l1->val < l2->val) {
+      l1->next = merge(l1->next, l2);
+      return l1;
+    } else {
+      l2->next = merge(l1, l2->next);
+      return l2;
+    }
+  }
 };
