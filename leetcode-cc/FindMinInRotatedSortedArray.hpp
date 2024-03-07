8b19eb8a87ef52d06ca76dfa0ade07e09aa6192f
SKTT1Ryze <ccc@neos.moe>
2024-03-07 10:38:55 +0800

    impl find min in rotated sorted array

--- leetcode-cc/FindMinInRotatedSortedArray.hpp
+++ leetcode-cc/FindMinInRotatedSortedArray.hpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -9,7 +11,7 @@ IMPLEMENT_PROBLEM_CLASS(PFindMinInRotatedSortedArray, 153, DIFFI_MEDIUM,
                         "Find Minimum in Rotated Sorted Array",
                         "Given the sorted rotated array nums of unique "
                         "elements, return the minimum element of this array.",
-                        {""});
+                        {"Array"});
 
 class SFindMinInRotatedSortedArray : public ISolution {
  public:
@@ -20,11 +22,33 @@ class SFindMinInRotatedSortedArray : public ISolution {
   string location() const override { return __FILE_NAME__; }
   int test() const override {
     return testHelper<vector<int>, int>(
-        {{3, 4, 5, 1, 2}, {4, 5, 6, 7, 0, 1, 2}, {11, 13, 15, 17}}, {1, 0, 11},
-        [this](auto input) { return this->findMin(input); });
+        {{3, 4, 5, 1, 2}, {4, 5, 6, 7, 0, 1, 2}, {11, 13, 15, 17}, {2, 1}},
+        {1, 0, 11, 1}, [this](auto input) { return this->findMin(input); });
   };
   int benchmark() const override { return 0; }
 
  private:
-  int findMin(vector<int>& nums) const {}
+  int findMin(vector<int>& nums) const {
+    int n = nums.size();
+    int start = 0;
+    int end = n - 1;
+
+    int res = INT_MAX;
+
+    while (start <= end) {
+      int middle = (start + end) / 2;
+
+      if (nums[start] <= nums[middle]) {
+        // start..middle is sorted
+        res = min(res, nums[start]);
+        start = middle + 1;
+      } else {
+        // middle..end is sorted
+        res = min(res, nums[middle]);
+        end = middle - 1;
+      }
+    }
+
+    return res;
+  }
 };
