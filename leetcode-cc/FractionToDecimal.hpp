2c87c3e35fafe3dac2e74c591c3a5a5172c41164
SKTT1Ryze <ccc@neos.moe>
2024-03-09 10:29:42 +0800

    impl fraction to decimal

--- leetcode-cc/FractionToDecimal.hpp
+++ leetcode-cc/FractionToDecimal.hpp
@@ -1,3 +1,5 @@
+#include <unordered_map>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -28,5 +30,39 @@ class SFractionToDecimal : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  string fractionToDecimal(int numerator, int denominator) const {}
+  string fractionToDecimal(int numerator, int denominator) const {
+    if (numerator == 0) return "0";
+
+    string result;
+
+    if ((numerator < 0) ^ (denominator < 0)) result += '-';
+
+    long long num = abs((long long)numerator);
+    long long den = abs((long long)denominator);
+
+    result += to_string(num / den);
+
+    long long remainder = num % den;
+    if (remainder == 0) return result;
+
+    result += '.';
+
+    unordered_map<long long, int> map;
+    while (remainder != 0) {
+      // begin recurring
+      if (map.find(remainder) != map.end()) {
+        result.insert(map[remainder], "(");
+        result += ')';
+        break;
+      }
+
+      map[remainder] = result.size();
+
+      remainder *= 10;
+      result += to_string(remainder / den);
+      remainder %= den;
+    }
+
+    return result;
+  }
 };
