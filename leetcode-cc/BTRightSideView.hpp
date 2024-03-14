e64c15621bcce4f865660013416c46523c178b63
SKTT1Ryze <ccc@neos.moe>
2024-03-14 15:21:58 +0800

    impl binary tree right side view

--- leetcode-cc/BTRightSideView.hpp
+++ leetcode-cc/BTRightSideView.hpp
@@ -1,3 +1,5 @@
+#include <stack>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -20,11 +22,40 @@ class SBTRightSideView : public ISolution {
   string location() const override { return __FILE_NAME__; }
   int test() const override {
     return testHelperBinaryTreeV2<vector<int>>(
-        {"[1,2,3,null,5,null,4]", "[1,null,3]", "[]"}, {{1, 3, 4}, {1, 3}, {}},
+        {"[1,2,3,null,5,null,4]", "[1,null,3]", "[]", "[1,2,3,null,5,6]"},
+        {{1, 3, 4}, {1, 3}, {}, {1, 3, 6}},
         [this](auto root) { return this->rightSideView(root); });
   };
   int benchmark() const override { return 0; }
 
  private:
-  vector<int> rightSideView(TreeNode* root) const {}
+  vector<int> rightSideView(TreeNode* root) const {
+    stack<pair<TreeNode*, int>> s = {};
+    int maxLevel = 0;
+    int currentLevel = 1;
+    auto node = root;
+    vector<int> res = {};
+
+    while (node || !s.empty()) {
+      if (node) {
+        if (currentLevel > maxLevel) {
+          res.push_back(node->val);
+          maxLevel++;
+        }
+        s.push({node, currentLevel});
+        node = node->right;
+        currentLevel++;
+      } else {
+        auto e = s.top();
+        s.pop();
+        node = e.first;
+        currentLevel = e.second;
+
+        node = node->left;
+        currentLevel++;
+      }
+    }
+
+    return res;
+  }
 };
