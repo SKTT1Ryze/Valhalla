964de8fcb4e5e3042d757737d3b93d86e86ff3c6
SKTT1Ryze <ccc@neos.moe>
2024-03-14 10:23:04 +0800

    impl repeated dna seq

--- leetcode-cc/RepeatedDNASeq.hpp
+++ leetcode-cc/RepeatedDNASeq.hpp
@@ -1,3 +1,5 @@
+#include <unordered_set>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -10,7 +12,7 @@ IMPLEMENT_PROBLEM_CLASS(
     "Given a string s that represents a DNA sequence, return all the "
     "10-letter-long sequences (substrings) that occur more than once in a DNA "
     "molecule. You may return the answer in any order.",
-    {""});
+    {"Hash Table & Sliding Window"});
 
 class SRepeatedDNASeq : public ISolution {
  public:
@@ -22,11 +24,29 @@ class SRepeatedDNASeq : public ISolution {
   int test() const override {
     return testHelper<string, vector<string>>(
         {"AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT", "AAAAAAAAAAAAA"},
-        {{"AAAAACCCCC", "CCCCCAAAAA"}, {"AAAAAAAAAA"}},
+        {{"CCCCCAAAAA", "AAAAACCCCC"}, {"AAAAAAAAAA"}},
         [this](auto input) { return this->findRepeatedDnaSequences(input); });
   };
   int benchmark() const override { return 0; }
 
  private:
-  vector<string> findRepeatedDnaSequences(string s) const {}
+  vector<string> findRepeatedDnaSequences(string s) const {
+    const int N = 10;
+    int n = s.size();
+    if (n < N) return {};
+
+    unordered_set<string> res = {};
+    unordered_set<string> memo = {};
+
+    for (int i = 0; i <= n - N; i++) {
+      auto subs = s.substr(i, N);
+      if (memo.contains(subs)) {
+        res.insert(subs);
+      } else {
+        memo.insert(subs);
+      }
+    }
+
+    return vector(res.begin(), res.end());
+  }
 };
