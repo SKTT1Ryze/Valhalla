cda8354df0dbc23b4d095b37a1121b6fb60f758c
SKTT1Ryze <ccc@neos.moe>
2024-03-19 10:35:19 +0800

    add number of islands

---
+++ leetcode-cc/NumOfIslands.hpp
@@ -0,0 +1,34 @@
+#include "TestHelper.h"
+#include "problem.h"
+#include "solution.h"
+
+using namespace std;
+
+IMPLEMENT_PROBLEM_CLASS(
+    PNumOfIslands, 200, DIFFI_MEDIUM, TOPIC_ALGORITHMS, "Number of Islands",
+    "Given an m x n 2D binary grid grid which represents a map of '1's (land) "
+    "and '0's (water), return the number of islands.",
+    {"Matrix"});
+
+class SNumOfIslands : public ISolution {
+ public:
+  size_t problemId() const override { return 200; }
+  string name() const override {
+    return ("Solution for " + string("Number of Islands"));
+  }
+  string location() const override { return __FILE_NAME__; }
+  int test() const override {
+    return testHelper<vector<vector<char>>, int>(
+        {{
+            {'1', '1', '1', '1', '0'},
+            {'1', '1', '0', '1', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '0', '0', '0'},
+        }},
+        {1}, [this](auto input) { return this->numIslands(input); });
+  };
+  int benchmark() const override { return 0; }
+
+ private:
+  int numIslands(vector<vector<char>>& grid) const {}
+};
