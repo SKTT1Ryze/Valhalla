6e254f90884a01fb8497b7278985ba041dac79cc
SKTT1Ryze <ccc@neos.moe>
2024-03-20 10:51:28 +0800

    impl dungeon game

--- leetcode-cc/DungeonGame.hpp
+++ leetcode-cc/DungeonGame.hpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <cmath>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -25,5 +28,28 @@ class SDungeonGame : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  int calculateMinimumHP(vector<vector<int>>& dungeon) const {}
+  int calculateMinimumHP(vector<vector<int>>& dungeon) const {
+    int m = dungeon.size();
+    int n = dungeon[0].size();
+
+    vector<vector<int>> dp(m, vector(n, 0));
+
+    dp[m - 1][n - 1] = max(1, 1 - dungeon[m - 1][n - 1]);
+
+    for (int i = n - 2; i >= 0; i--)
+      dp[m - 1][i] = max(1, dp[m - 1][i + 1] - dungeon[m - 1][i]);
+
+    for (int i = m - 2; i >= 0; i--)
+      dp[i][n - 1] = max(1, dp[i + 1][n - 1] - dungeon[i][n - 1]);
+
+    for (int i = m - 2; i >= 0; i--) {
+      for (int j = n - 2; j >= 0; j--) {
+        int down = max(1, dp[i + 1][j] - dungeon[i][j]);
+        int right = max(1, dp[i][j + 1] - dungeon[i][j]);
+        dp[i][j] = min(down, right);
+      }
+    }
+
+    return dp[0][0];
+  }
 };
