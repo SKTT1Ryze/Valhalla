0ec4531e5bd6cdd5bd91c149ec922bb1ed9a4cdd
SKTT1Ryze <ccc@neos.moe>
2024-03-21 10:35:09 +0800

    impl count primes

--- leetcode-cc/CountPrimes.hpp
+++ leetcode-cc/CountPrimes.hpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -18,11 +20,27 @@ class SCountPrimes : public ISolution {
   }
   string location() const override { return __FILE_NAME__; }
   int test() const override {
-    return testHelper<int, int>(
-        {}, {}, [this](auto input) { return this->countPrimes(input); });
+    return testHelper<int, int>({10, 0, 1}, {4, 0, 0}, [this](auto input) {
+      return this->countPrimes(input);
+    });
   };
   int benchmark() const override { return 0; }
 
  private:
-  int countPrimes(int n) const {}
+  int countPrimes(int n) const {
+    if (n < 2) return 0;
+    vector<bool> v(n, true);
+    v[0] = false;
+    v[1] = false;
+
+    for (int i = 2; i * i < n; i++) {
+      if (v[i]) {
+        for (int j = i * i; j < n; j += i) {
+          v[j] = false;
+        }
+      }
+    }
+
+    return count_if(v.begin(), v.end(), [](bool e) { return e; });
+  }
 };
