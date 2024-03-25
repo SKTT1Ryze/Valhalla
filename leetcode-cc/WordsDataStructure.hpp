fc7362f8c26261f2800093f23d666fe38615ef68
SKTT1Ryze <ccc@neos.moe>
2024-03-25 10:48:10 +0800

    impl word data structure

--- leetcode-cc/WordsDataStructure.hpp
+++ leetcode-cc/WordsDataStructure.hpp
@@ -1,3 +1,5 @@
+#include <unordered_map>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -9,7 +11,7 @@ IMPLEMENT_PROBLEM_CLASS(
     "Design Add and Search Words Data Structure",
     "Design a data structure that supports adding new words and finding if a "
     "string matches any previously added string.",
-    {""});
+    {"Trie"});
 
 class SWordsDataStructure : public ISolution {
  public:
@@ -23,12 +25,52 @@ class SWordsDataStructure : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
+  struct TrieNode {
+    char ch;
+    unordered_map<char, TrieNode*> nexts;
+    bool existEnd;
+
+    TrieNode() : ch(' '), nexts({}), existEnd(false) {}
+    TrieNode(char ch) : ch(ch), nexts({}), existEnd(false) {}
+  };
+
   class WordDictionary {
    public:
-    WordDictionary() {}
+    WordDictionary() { root = new TrieNode(); }
 
-    void addWord(string word) {}
+    void addWord(string word) {
+      auto p = root;
 
-    bool search(string word) {}
+      for (const auto& ch : word) {
+        if (!p->nexts.contains(ch)) {
+          auto newNode = new TrieNode(ch);
+          p->nexts.insert({ch, newNode});
+        }
+
+        p = p->nexts[ch];
+      }
+
+      p->existEnd = true;
+    }
+
+    bool search(string word) { return this->searchHelper(word, 0, root); }
+
+   private:
+    bool searchHelper(string& word, int start, TrieNode* p) {
+      if (start == word.size()) return p->existEnd;
+      char curr = word[start];
+      if (curr == '.') {
+        for (const auto& [ch, next] : p->nexts) {
+          if (this->searchHelper(word, start + 1, next)) return true;
+        }
+        return false;
+      } else if (p->nexts.contains(curr)) {
+        return this->searchHelper(word, start + 1, p->nexts[curr]);
+      } else {
+        return false;
+      }
+    }
+
+    TrieNode* root;
   };
 };
