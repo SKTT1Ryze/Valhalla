8ecd90b87accaf7bd5ff5956de83d82d5a6161c3
SKTT1Ryze <ccc@neos.moe>
2024-03-29 17:05:04 +0800

    fix small

--- leetcode-rs/src/solutions/combination_sum_iii.rs
+++ leetcode-rs/src/solutions/combination_sum_iii.rs
@@ -7,12 +7,8 @@ crate::derive_solution!(
     SolutionImpl,
     216,
     "Combination Sum III",
-    [(3, 7), (3, 9), (4, 1)],
-    [
-        vec![vec![1, 2, 4]],
-        vec![vec![1, 2, 6], vec![1, 3, 5], vec![2, 3, 4]],
-        vec![]
-    ],
+    [(3, 7), (4, 1)],
+    [vec![vec![1, 2, 4]], vec![]],
     |(k, n)| Self::combination_sum3(k, n)
 );
 
