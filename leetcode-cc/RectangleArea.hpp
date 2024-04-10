809f1d95d42250e1ad330003b1b367a9113eef5c
SKTT1Ryze <ccc@neos.moe>
2024-04-10 17:21:22 +0800

    impl rectangle area

--- leetcode-cc/RectangleArea.hpp
+++ leetcode-cc/RectangleArea.hpp
@@ -8,7 +8,7 @@ IMPLEMENT_PROBLEM_CLASS(
     PRectangleArea, 223, DIFFI_MEDIUM, TOPIC_ALGORITHMS, "Rectangle Area",
     "Given the coordinates of two rectilinear rectangles in a 2D plane, return "
     "the total area covered by the two rectangles.",
-    {""});
+    {"Math"});
 
 class SRectangleArea : public ISolution {
  public:
@@ -26,5 +26,13 @@ class SRectangleArea : public ISolution {
 
  private:
   int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2,
-                  int by2) const {}
+                  int by2) const {
+    int areaA = (ax2 - ax1) * (ay2 - ay1);
+    int areaB = (bx2 - bx1) * (by2 - by1);
+
+    int areaOverlapX = max(0, min(ax2, bx2) - max(ax1, bx1));
+    int areaOverlapY = max(0, min(ay2, by2) - max(ay1, by1));
+
+    return areaA + areaB - areaOverlapX * areaOverlapY;
+  }
 };
