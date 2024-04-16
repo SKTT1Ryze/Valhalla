520ef83bc4939f539ce84944dba41174095d669b
SKTT1Ryze <ccc@neos.moe>
2024-04-16 11:08:59 +0800

    impl power of two

--- leetcode-rs/src/solutions/power_of_two.rs
+++ leetcode-rs/src/solutions/power_of_two.rs
@@ -13,6 +13,12 @@ crate::derive_solution!(
 
 impl SolutionImpl {
     pub fn is_power_of_two(n: i32) -> bool {
-        todo!()
+        if n == 1 {
+            true
+        } else if n == 0 {
+            false
+        } else {
+            n % 2 == 0 && Self::is_power_of_two(n / 2)
+        }
     }
 }
