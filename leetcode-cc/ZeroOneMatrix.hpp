e0dc61063bfc3f8058b9690b7ebf6aca5623a6c5
SKTT1Ryze <ccc@neos.moe>
2024-04-24 10:44:13 +0800

    impl zero one matrix

--- leetcode-cc/ZeroOneMatrix.hpp
+++ leetcode-cc/ZeroOneMatrix.hpp
@@ -1,3 +1,7 @@
+#include <climits>
+#include <queue>
+#include <utility>
+
 #include "TestHelper.h"
 #include "problem.h"
 #include "solution.h"
@@ -30,5 +34,39 @@ class SZeroOneMatrix : public ISolution {
   int benchmark() const override { return 0; }
 
  private:
-  vector<vector<int>> updateMatrix(vector<vector<int>>& mat) const {}
+  vector<vector<int>> updateMatrix(vector<vector<int>>& mat) const {
+    int m = mat.size();
+    int n = mat[0].size();
+
+    vector<vector<int>> dis(m, vector(n, INT_MAX));
+    queue<pair<int, int>> q = {};
+    vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (mat[i][j] == 0) {
+          dis[i][j] = 0;
+          q.push({i, j});
+        }
+      }
+    }
+
+    while (!q.empty()) {
+      auto [currentX, currentY] = q.front();
+      q.pop();
+
+      for (const auto& [dirX, dirY] : dirs) {
+        auto nextX = currentX + dirX;
+        auto nextY = currentY + dirY;
+
+        if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n &&
+            dis[nextX][nextY] > dis[currentX][currentY] + 1) {
+          dis[nextX][nextY] = dis[currentX][currentY] + 1;
+          q.push({nextX, nextY});
+        }
+      }
+    }
+
+    return std::move(dis);
+  }
 };
